f67cc805cac31e6ce1127e323afb87b65d3df48a
shun771210 <water25234@hotmail.com>
2018-11-10 18:21:19 +0800

    fix index.js logic

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const seoParser = require('./models/seo-parser');
 
-parser();
+function parser(params = {}) {
+	const filePath = params.file ? params.fil : './template/template.html';
+	const options = params.options;
 
-function parser() {
-	const filePath = './template/template.html';
-
-	const parser2 = new (seoParser)(filePath);
+	const parser2 = new (seoParser)(filePath, options);
+	
 	parser2
 		.seoRulerProcess()
 		.consoleLog();
 }
+
+exports = module.exports = {
+	parser: parser
+};
